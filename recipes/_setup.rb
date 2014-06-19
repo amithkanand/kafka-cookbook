e9d14651b6407c37dca3146d730ed7aba4860ab1
Amit Anand <anandkamith@gmail.com>
2014-06-19 09:47:03 -0400

    Fixed _setup.rb to set correct owner for Zookeeper data directory and modified zookeeper template to set a zookeeper quorum

--- recipes/_setup.rb
+++ recipes/_setup.rb
@@ -16,6 +16,7 @@ end
   node[:kafka][:config_dir],
   node[:kafka][:log_dir],
   node[:kafka][:build_dir]
+  node[:zookeeper][:data_dir]
 ].each do |dir|
   directory dir do
     owner node[:kafka][:user]
